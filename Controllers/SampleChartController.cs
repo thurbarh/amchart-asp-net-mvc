f8e4fdb42eb366f314ffae9f148b67cda7296f7d
toba <atoba@simplexsystem.com>
2018-01-18 15:58:31 +0100

    add samplechart to repo

---
+++ Controllers/SampleChartController.cs
@@ -0,0 +1,40 @@
+﻿using SampleChart.DataModel;
+using SampleChart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleChart.Controllers
+{
+    public class SampleChartController : Controller
+    {
+        //create instance of datacontext
+        SampleDbEntities db = new SampleDbEntities();
+
+        // GET: SampleChart
+        public ActionResult AmChartSample()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetChartData()
+        {
+            var model = db.Prices
+                .ToList()
+                .Select(m => new ChartDataDTO
+                 {
+                    Id=m.ID,
+                    OfferPrice=m.OfferPrice.HasValue?m.OfferPrice.Value:0,
+                    BidPrice= m.BidPrice.HasValue ? m.BidPrice.Value : 0,
+                    EffectiveYield=m.EffectiveYield.HasValue?m.EffectiveYield.Value:0,
+                    Date=m.Date.Value.ToString("yyyy-MM-dd")
+                }).OrderBy(j=>j.Date).ToList();
+
+            return Json(model.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Models/ChartDataDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleChart.Models
+{
+    public class ChartDataDTO
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public decimal OfferPrice { get; set; }
+        public decimal BidPrice { get; set; }
+        public decimal EffectiveYield { get; set; }
+    }
+}
